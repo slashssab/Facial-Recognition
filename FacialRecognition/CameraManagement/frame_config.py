ea93300da3a53a847c7bd886d49c331ff0f086f1
Michał Wojciechowski <226678@student.pwr.edu.pl>
2018-03-10 22:32:55 +0100

    Commit

--- FacialRecognition/CameraManagement/frame_config.py
+++ FacialRecognition/CameraManagement/frame_config.py
@@ -1,11 +1,11 @@
 """Import modules"""
 import cv2
 
-def draw_text(img, text, x, y):
+def draw_text(img, text, x_, y_):
     """Simply draw the text"""
-    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+    cv2.putText(img, text, (x_, y_), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
-def display_frame(faces, frame, text): 
+def display_frame(faces, frame, text):
     """Draws a frame with text around the face"""
     for (x, y, w, h) in faces: 
         cv2.rectangle(frame,(x, y),(x + w, y + h),(255, 0, 0), 2)
--- FacialRecognition/ViedoCapture.py
+++ FacialRecognition/ViedoCapture.py
@@ -1,7 +1,6 @@
 from CameraManagement.camera_manager import *
 from CameraManagement.frame_config import *
 from Teaching.teacher_help import setup_face_database
-
 import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -24,7 +23,7 @@ while(True):
     # Display the resulting frame
     display_frame(faces, frame, "tekst z bazy"+str(imagOrd))
     
-    # Take a photo of your face, and save in the images folder
+    # Take a photo of your face, and save It in the "images" folder
     if cv2.waitKey(1) & 0xff == ord('k'):
         setup_face_database(frame, cap, file, imagOrd)
         imagOrd = imagOrd + 1
@@ -35,4 +34,5 @@ while(True):
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+input()
 
