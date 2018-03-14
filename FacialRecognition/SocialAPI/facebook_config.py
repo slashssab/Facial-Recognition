3472e5699065720cb6b66dc7c4ea4b9fe7735f88
manioo8 <manioo8@DESKTOP-112CE2D>
2018-03-14 17:53:56 +0100

    jest szansa że działa

--- FacialRecognition/SocialAPI/facebook_config.py
+++ FacialRecognition/SocialAPI/facebook_config.py
@@ -70,6 +70,9 @@ import urllib
 import urllib.parse
 import subprocess
 import warnings
+import pycurl
+import io
+import certifi
 
 # Hide deprecation warnings. The facebook module isn't that up-to-date (facebook.GraphAPIError).
 warnings.filterwarnings('ignore', category=DeprecationWarning)
@@ -85,24 +88,42 @@ FACEBOOK_PROFILE_ID = '100024705713078'
 oauth_args = dict(client_id     = FACEBOOK_APP_ID,
                   client_secret = FACEBOOK_APP_SECRET,
                   grant_type    = 'client_credentials')
-oauth_curl_cmd = ['curl',
-                  'https://graph.facebook.com/oauth/access_token?' + urllib.parse.urlencode(oauth_args)]
-oauth_response = subprocess.Popen(oauth_curl_cmd,
-                                  stdout = subprocess.PIPE,
-                                  stderr = subprocess.PIPE).communicate()[0]
+#oauth_curl_cmd = ['curl',
+#                  'https://graph.facebook.com/oauth/access_token?' + urllib.parse.urlencode(oauth_args)]
 
-try:
-    oauth_access_token = urllib.parse.parse_qs(str(oauth_response))['access_token'][0]
-except KeyError:
-    print('Unable to grab an access token!')
-    exit()
+
+buffer=io.BytesIO()
+c = pycurl.Curl()
+c.setopt(pycurl.CAINFO, certifi.where())
+c.setopt(c.URL, 'https://graph.facebook.com/oauth/access_token?' + urllib.parse.urlencode(oauth_args))
+c.setopt(c.WRITEDATA, buffer)
+c.perform()
+c.close()
+
+body = buffer.getvalue()
+print(body)
+
+#oauth_response = subprocess.Popen(oauth_curl_cmd,
+#                                  stdout = subprocess.PIPE,
+#                                  stderr = subprocess.PIPE, shell=True).communicate()[1]
+#oauth_access_token = urllib.parse.parse_qs(body)["access_token"]
+#try:
+body1=str(body)
+oauth_access_token = body1.split('"')[3]
+
+#    oauth_access_token = urllib.parse.parse_qs(body)['access_token']
+#    input()
+#except KeyError:
+#    print('Unable to grab an access token!')
+#    input()
+#    exit()
 
 facebook_graph = facebook.GraphAPI(oauth_access_token)
 
-## Try to post something on the wall.
+# Try to post something on the wall.
 #try:
-#    fb_response = facebook_graph.put_wall_post('Hello from Python', \
-#                                               profile_id = FACEBOOK_PROFILE_ID)
-#    print fb_response
+fb_response = facebook_graph.put_wall_post('Hello from Python', \
+                                               profile_id = FACEBOOK_PROFILE_ID)
+#    print(fb_response)
 #except facebook.GraphAPIError as e:
-#    print 'Something went wrong:', e.type, e.message
+#    print('Something went wrong:'), e.type, e.message
